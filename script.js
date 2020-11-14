6205d6017f2dc288bdd1a4230903883f189921de
Katherine Nguyen <nguyenkat01@yahoo.com>
2020-11-14 11:37:59 -0800

    Added Tours modal

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+var toursModal = $("#tours");
 var newsModal = $("#news");
 var photosModal = $("#photos");
 var contactModal = $("#contact");
+var modalBgTours = $(".modal-bg-tours");
 var modalBgNews = $(".modal-bg-news");
 var modalBgPhotos = $(".modal-bg-photos");
 var modalBgContact = $(".modal-bg-contact");
 var modalClose = $(".modal-close");
+
+toursModal.on("click", function () {
+    modalBgTours.addClass("bg-active");
+});
 
 newsModal.on("click", function () {
     modalBgNews.addClass("bg-active");
@@ -19,6 +25,7 @@ contactModal.on("click", function () {
 });
 
 modalClose.on("click", function () {
+    modalBgTours.removeClass("bg-active");
     modalBgNews.removeClass("bg-active");
     modalBgPhotos.removeClass("bg-active");
     modalBgContact.removeClass("bg-active");
